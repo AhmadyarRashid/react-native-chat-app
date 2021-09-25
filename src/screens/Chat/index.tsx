01385b1b6ba32a38c934cad79445f214889f4d60
AhmadyarRashid <ahmadyarmeo@gmail.com>
2021-09-25 21:59:35 +0500

    create new project

---
+++ src/screens/Chat/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export type Props = {
+  name?: string;
+  baseEnthusiasmLevel?: number;
+};
+
+const ChatScreen: React.FC<Props> = () => {
+  return(
+    <View>
+      <Text>This is chat app</Text>
+    </View>
+  );
+};
+
+export default ChatScreen;
